b8d31587a0eeb825232c38b5d71439ed0725a330
esther <estherchaya18@gmail.com>
2019-05-28 20:37:56 +0300

    styles changes

---
+++ TravelAgency/wwwroot/js/search.js
@@ -0,0 +1,9 @@
+﻿
+
+$(document).ready(function () {
+    $('#name').keyup(function (e) {
+        if (e.which == 13)
+            $('form').submit();
+        //$('form').load('/phones/search/?name='+$(this).val);
+    })
+});
